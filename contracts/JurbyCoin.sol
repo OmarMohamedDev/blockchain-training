e16ea0484b55bb91d564e6dc96e26315dfd1944a
Omar Mohamed <omarmohamed@Omars-MacBook.fritz.box>
2019-01-24 19:09:53 +0100

    playing with jurbycoin 'theft' contract

---
+++ contracts/JurbyCoin.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.0;
+
+pragma solidity >=0.4.21 <0.6.0;
+
+contract JurbyCoin {
+    address payable public CEO;
+    address payable public CTO;
+    address payable public COO;
+
+    constructor (address payable _CEO, address payable _CTO, address payable _COO) public {
+        CEO = _CEO;
+        CTO = _CTO;
+        COO = _COO;
+    }
+
+    function donate() payable public {}
+
+    function buy() payable public {
+        CEO.transfer(msg.value / 2);
+        CTO.transfer(msg.value / 4);
+        COO.transfer(msg.value / 4);
+    }
+}
